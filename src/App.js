44cbcc13e5f1cdfabe9383b35bb2b60134bec3a9
Robert Martins <robert.rm131@gmail.com>
2021-12-08 17:23:26 -0300

    Adiciona validação nos campos de login, preenche o localStorage  e redireciona para a página de receitas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './context/RecipesProvider';
 import LoginPage from './pages/LoginPage';
+import Meals from './pages/Meals';
 
 function App() {
   return (
@@ -18,7 +20,12 @@ function App() {
     //   </object>
     // </div>
     <RecipesProvider>
-      <LoginPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ LoginPage } />
+          <Route exact path="/comidas" component={ Meals } />
+        </Switch>
+      </BrowserRouter>
     </RecipesProvider>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function LoginPage() {
+function LoginPage({ history }) {
+  const [email, setEmail] = useState({
+    email: '',
+  });
+  const [password, setPassword] = useState({
+    password: '',
+  });
+
+  useEffect(() => {
+    localStorage.setItem('mealsToken', JSON.stringify(null));
+    localStorage.setItem('cocktailsToken', JSON.stringify(null));
+  }, []);
+
+  function validateLogin() {
+    const regexEmail = /\S+@\S+\.\S+/;
+    const MIN_PASSWORD = 7;
+    const validatingEmail = regexEmail.test(email.email);
+    const validatingPassword = password.password.length >= MIN_PASSWORD;
+    const validatingInputs = (validatingEmail && validatingPassword);
+    return !validatingInputs;
+  }
+
+  function onSubmitLogin(e) {
+    e.preventDefault();
+    localStorage.setItem('mealsToken', JSON.stringify(1));
+    localStorage.setItem('cocktailsToken', JSON.stringify(1));
+    localStorage.setItem('user', JSON.stringify(email));
+    history.push('/comidas');
+  }
+
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={ (e) => onSubmitLogin(e) }>
       <label htmlFor="email">
         <input
           data-testid="email-input"
           id="email"
+          onChange={ ({ target }) => setEmail({ email: target.value }) }
           placeholder="email"
           type="email"
+          value={ email.email }
         />
       </label>
       <label htmlFor="password">
         <input
           data-testid="password-input"
           id="password"
+          onChange={ ({ target }) => setPassword({ password: target.value }) }
           placeholder="password"
           type="password"
+          value={ password.password }
         />
       </label>
       <button
         data-testid="login-submit-btn"
+        disabled={ validateLogin() }
         type="submit"
       >
         Login
@@ -28,5 +63,9 @@ function LoginPage() {
     </form>
   );
 }
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({}),
+}.isRequired;
 
 export default LoginPage;
