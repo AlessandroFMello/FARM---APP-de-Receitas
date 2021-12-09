5b019dc35817ea95f7b680af1bb2be2c10a7a898
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-12-09 03:43:29 -0300

    Requisito 14 inacabado

--- src/components/header-components/SearchBar.jsx
+++ src/components/header-components/SearchBar.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+// import fetchAPI from '../../services/fetchAPI';
 
-export default function SearchBar() {
+function SearchBar() {
+  function getRadioEndpoint() {
+    const radios = [...document.querySelectorAll('.search-bar-radio')];
+    const checkedRadio = radios.find((radio) => radio.checked);
+    return checkedRadio.value;
+  }
+
   return (
     <div>
       <form className="search-bar">
@@ -10,8 +17,48 @@ export default function SearchBar() {
             data-testid="search-input"
           />
         </label>
-        <button type="button">Buscar</button>
+        <label htmlFor="search-bar-radio">
+          <input
+            data-testid="ingredient-search-radio"
+            className="search-bar-radio"
+            name="search-bar-radio"
+            type="radio"
+            value="ingrediente"
+          />
+          Ingrediente
+        </label>
+        <label htmlFor="search-bar-radio">
+          <input
+            data-testid="name-search-radio"
+            className="search-bar-radio"
+            name="search-bar-radio"
+            type="radio"
+            value="nome"
+          />
+          Nome
+        </label>
+        <label htmlFor="search-bar-radio">
+          <input
+            data-testid="first-letter-search-radio"
+            className="search-bar-radio"
+            name="search-bar-radio"
+            type="radio"
+            value="primeira-letra"
+          />
+          Primeira Letra
+        </label>
+
+        <button
+          data-testid="exec-search-btn"
+          onClick={ () => { console.log(getRadioEndpoint()); } }
+          type="button"
+        >
+          Buscar
+
+        </button>
       </form>
     </div>
   );
 }
+
+export default SearchBar;
