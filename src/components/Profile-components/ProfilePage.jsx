451b5718c9979634b4d4109372b3d87f4f0549c2
Fernando Segregio <segregio@gmail.com>
2021-12-15 14:48:23 -0300

    Cria campo de email na tela, cria botões de Receitas Favoritas, Receitas Feitas e Sair.

---
+++ src/components/Profile-components/ProfilePage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+function ProfilePage() {
+  function recoverFromLocalStorage() {
+    const emailLocalStorage = JSON.parse(localStorage.getItem('user'));
+    return emailLocalStorage.email;
+  }
+
+  return (
+    <div className="profile-page-container">
+      <div data-testid="profile-email">
+        {recoverFromLocalStorage()}
+      </div>
+      <Link to="/receitas-feitas">
+        <Button
+          data-testid="profile-done-btn"
+          type="button"
+          variant="secondary"
+        >
+          Receitas Feitas
+        </Button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <Button
+          data-testid="profile-favorite-btn"
+          type="button"
+          variant="secondary"
+        >
+          Receitas Favoritas
+        </Button>
+      </Link>
+      <Link to="/">
+        <Button
+          data-testid="profile-logout-btn"
+          type="button"
+          variant="secondary"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </Button>
+
+      </Link>
+    </div>
+  );
+}
+
+export default ProfilePage;
