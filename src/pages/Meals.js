d68c2bf0ad8b7a8755bef141c79d84b6d1bdc5f0
Matheus Santos <matheusgabriel.18@gmail.com>
2021-12-19 13:44:48 -0300

    Cria o componente com as categorias

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import RecipesCards from '../components/body-components/RecipesCards';
+import RecipesCategories from '../components/body-components/RecipesCategories';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -10,6 +11,7 @@ function Meals() {
   return (
     <div>
       <Header pageName={ pageName } />
+      <RecipesCategories recipeType="meals" />
       <RecipesCards recipeType="meals" />
       <Footer />
     </div>
