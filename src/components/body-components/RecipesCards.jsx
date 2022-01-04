4d5d5cc46527b3e246fa85a7ff375edb5dd00887
Fernando Segregio <segregio@gmail.com>
2022-01-04 20:46:44 -0300

    Cria UseCallback, Correção condicional de recebimento do array recipes.

--- src/components/body-components/RecipesCards.jsx
+++ src/components/body-components/RecipesCards.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from '../../context/RecipesContext';
 
@@ -10,43 +10,57 @@ function RecipesCards({ recipeType }) {
     console.log('[RecipesCards] initialFetch');
   }, [initialFetch]);
 
-  function getRecipeThumb(recipe) {
-    if (recipeType === 'drinks') return recipe.strDrinkThumb;
-    if (recipeType === 'meals') return recipe.strMealThumb;
-  }
+  const getRecipeCards = useCallback(
+    () => {
+      function getRecipeThumb(recipe) {
+        if (recipeType === 'drinks') return recipe.strDrinkThumb;
+        if (recipeType === 'meals') return recipe.strMealThumb;
+      }
 
-  function getRecipeName(recipe) {
-    if (recipeType === 'drinks') return recipe.strDrink;
-    if (recipeType === 'meals') return recipe.strMeal;
-  }
+      function getRecipeName(recipe) {
+        if (recipeType === 'drinks') return recipe.strDrink;
+        if (recipeType === 'meals') return recipe.strMeal;
+      }
 
-  function getRecipeId(recipe) {
-    if (recipeType === 'drinks') return recipe.idDrink;
-    if (recipeType === 'meals') return recipe.idMeal;
-  }
+      function getRecipeId(recipe) {
+        if (recipeType === 'drinks') return recipe.idDrink;
+        if (recipeType === 'meals') return recipe.idMeal;
+      }
 
-  function getRecipeCards() {
-    const MAX_CARDS = 12;
-    let recipesToRender = recipes[recipeType] || initialFetchObject[recipeType];
-    recipesToRender = recipesToRender.slice(0, MAX_CARDS);
+      const MAX_CARDS = 12;
 
-    return (
-      recipesToRender.map((recipe, index) => (
-        <div
-          className="recipe-card"
-          key={ getRecipeId(recipe) }
-          data-testid={ `${index}-recipe-card` }
-        >
-          <img
-            src={ getRecipeThumb(recipe) }
-            data-testid={ `${index}-card-img` }
-            alt={ getRecipeName(recipe) }
-          />
-          <h3 data-testid={ `${index}-card-name` }>{ getRecipeName(recipe) }</h3>
-        </div>
-      ))
-    );
-  }
+      let recipesToRender;
+
+      if (Object.keys(recipes).length > 0) {
+        recipesToRender = recipes;
+      } else if (initialFetchObject[recipeType]) {
+        recipesToRender = initialFetchObject[recipeType];
+      }
+
+      recipesToRender = recipesToRender.slice(0, MAX_CARDS);
+      return (
+        recipesToRender.map((recipe, index) => (
+          <div
+            className="recipe-card"
+            key={ getRecipeId(recipe) }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <img
+              src={ getRecipeThumb(recipe) }
+              data-testid={ `${index}-card-img` }
+              alt={ getRecipeName(recipe) }
+            />
+            <h3 data-testid={ `${index}-card-name` }>{ getRecipeName(recipe) }</h3>
+          </div>
+        ))
+      );
+    },
+    [recipes, initialFetchObject, recipeType],
+  );
+
+  useEffect(() => {
+    getRecipeCards();
+  }, [getRecipeCards, recipes]);
 
   return (
     <section className="recipe-cards">
