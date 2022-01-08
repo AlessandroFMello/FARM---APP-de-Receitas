0500ca4ce146bbfa651e142a3c757989d95d779b
Robert Rodrigues <robert.rm131@gmail.com>
2022-01-07 23:21:11 -0300

    realiza localStorage req 50 teste

--- src/components/body-components/IngredientsList.jsx
+++ src/components/body-components/IngredientsList.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import RecipesContext from '../../context/RecipesContext';
 
 function IngredientsList() {
@@ -9,17 +9,10 @@ function IngredientsList() {
     verifyIfAllIngredientsChecked,
   } = useContext(RecipesContext);
   const { pathname } = useLocation();
+  const [check, setCheck] = useState([]);
   const [ingredientsLength, setIngredientsLength] = useState(0);
   const [checkedIngredients, setCheckedIngredients] = useState(0);
-
-  useEffect(() => {
-    verifyIfAllIngredientsChecked(ingredientsLength, checkedIngredients);
-  }, [
-    verifyIfAllIngredientsChecked,
-    checkedIngredients,
-    ingredientsLength,
-    setDoneRecipe,
-  ]);
+  const params = useParams();
 
   function getIngredient() {
     const max = 20;
@@ -36,6 +29,38 @@ function IngredientsList() {
     console.log(pathname);
     return ingredientsArr;
   }
+
+  useEffect(() => {
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    const length = getIngredient();
+    if (inProgressRecipes === null) {
+      const first = {
+        cocktails: {},
+        meals: {}
+      }
+      return localStorage.setItem('inProgressRecipes', JSON.stringify(first))
+    }
+    const mealOrDrink = {
+      comidas: 'meals',
+      bebidas: 'cocktails'
+    }
+    const verifyType = pathname.match('bebidas') || pathname.match('comidas');
+    const { id } = params;
+    const type = verifyType[0];
+    const arrayOfIngredients = inProgressRecipes[mealOrDrink[type]][id];
+    if(arrayOfIngredients) {
+      setCheck([...arrayOfIngredients]);
+    }
+  }, []);
+
+  useEffect(() => {
+    verifyIfAllIngredientsChecked(ingredientsLength, checkedIngredients);
+  }, [
+    verifyIfAllIngredientsChecked,
+    checkedIngredients,
+    ingredientsLength,
+    setDoneRecipe,
+  ]);
 
   const ingredients = getIngredient();
 
@@ -56,17 +81,69 @@ function IngredientsList() {
   }
 
   function lineThroughIngredient({ target }) {
-    setIngredientsLength(ingredients.length);
+    const name = target.name;
     const father = target.parentElement;
     if (target.checked) {
       father.style.textDecoration = 'line-through';
+      setCheck((prev) => [...prev, name])
+      console.log(check.length)
       setCheckedIngredients((prev) => prev + 1);
+      const obj = {
+        comidas: 'meals',
+        bebidas: 'cocktails'
+      }
+      const { type } = recipe;
+      const id = params.id;
+      const inprogress = JSON.parse(localStorage.getItem('inProgressRecipes'))
+      let obj2 = {};
+      if (inprogress[obj[type]][id]) {
+        const prev = [...inprogress[obj[type]][id]];
+        obj2 = {
+          ...inprogress,
+          [obj[type]]: {
+            ...inprogress[obj[type]],
+            [id]: [...prev, target.name]
+          }
+        }
+      } else {
+        obj2 = {
+          ...inprogress,
+          [obj[type]]: {
+            ...inprogress[obj[type]],
+            [id]: [target.name]
+          }
+        }
+      }
+      localStorage.setItem('inProgressRecipes', JSON.stringify(obj2))
     } else if (!target.checked) {
+      console.log(check.length)
       father.style.textDecoration = 'none';
-      setCheckedIngredients((prev) => prev - 1);
+      const filtered = check.filter((el) => el !== name);
+      setCheck(filtered)
+      setCheckedIngredients((prev) => prev + 1);
+      const obj = {
+        comidas: 'meals',
+        bebidas: 'cocktails'
+      }
+      const { type } = recipe;
+      const id = params.id;
+      const inprogress = JSON.parse(localStorage.getItem('inProgressRecipes'))
+      let obj2 = {};
+      if (inprogress[obj[type]][id]) {
+        const prev = [...inprogress[obj[type]][id]];
+        const filter = prev.filter((el) => el !== name)
+        obj2 = {
+          ...inprogress,
+          [obj[type]]: {
+            ...inprogress[obj[type]],
+            [id]: filter
+          }
+        }
+      localStorage.setItem('inProgressRecipes', JSON.stringify(obj2))
     }
-  }
-
+  
+  }}
+  
   function getIngredientCheck() {
     return (
       <div
@@ -80,8 +157,11 @@ function IngredientsList() {
             data-testid={ `${index}-ingredient-step` }
           >
             <input
+              className="teste"
               type="checkbox"
+              defaultChecked={ check.includes(ingredient) }
               id={ `${ingredient}.${index}` }
+              name={ `${ingredient}` }
               onClick={ (e) => lineThroughIngredient(e) }
             />
             {`${ingredient} `}
