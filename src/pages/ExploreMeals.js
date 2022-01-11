6a26745d762c373a32ca4dc8aaf1c3d273270911
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-01-10 21:15:17 -0300

    Cria botões de explorar

--- src/pages/ExploreMeals.js
+++ src/pages/ExploreMeals.js
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import ExploreHeader from '../components/ExploreHeader';
 import Footer from '../components/Footer';
 
@@ -9,15 +10,38 @@ function ExploreMeals() {
   return (
     <div>
       <ExploreHeader pageName={ pageName } />
+      <div className="profile-page-container">
+        <Link to="/explorar/comidas">
+          <Button
+            data-testid="explore-by-ingredient"
+            type="button"
+            variant="outline-dark"
+          >
+            Por Ingredientes
+          </Button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <Button
+            data-testid="explore-by-area"
+            type="button"
+            variant="outline-dark"
+          >
+            Por Local de Origem
+          </Button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <Button
+            data-testid="explore-surprise"
+            type="button"
+            variant="outline-dark"
+          >
+            Me Surpreenda!
+          </Button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
 }
-
-ExploreMeals.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
-  }).isRequired,
-};
 
 export default ExploreMeals;
